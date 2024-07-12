3eb2074ffd844c9f1ae7ef5f8441764e6f003057
RomanBabych <rrroma.babi42017@gmail.com>
2024-07-13 00:05:28 +0300

    homework done2

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -11,13 +11,20 @@ export default function MovieCast() {
     fetchMovieCast(movieId).then((data) => setCast(data.cast));
   }, [movieId]);
 
+  const defaultImg =
+    "https://dummyimage.com/200x300/cdcdcd/000.jpg&text=No+Image";
+
   return (
     <ul className={css.castList}>
       {cast.map((actor) => (
         <li className={css.castListItem} key={actor.id}>
           <img
             className={css.castImage}
-            src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+            src={
+              actor.profile_path
+                ? `https://image.tmdb.org/t/p/w200${actor.profile_path}`
+                : defaultImg
+            }
             alt={actor.name}
           />
           <p>{actor.name}</p>
